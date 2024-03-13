83c3371afdd015fa7afa2d06ac4ef499a3829bf9
samuel <ssamuel211102@gmail.com>
2024-03-13 16:37:21 -0600

    fit: create component HelloWorld

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.astro'],
+	theme: {
+		extend: {
+			colors: {
+				primaryYellow: "#DBF227",
+				primaryBlue: "#042940",
+			}
+		},
+	},
+	plugins: [],
+}
